803c0c9aaeef7da9ecf887d66850c1f5c61679e3
HyunLee <chdnjf103@gmail.com>
2020-08-22 15:00:19 +0900

    updt var name

--- util.py
+++ util.py
@@ -9,16 +9,18 @@ from sklearn.model_selection import cross_val_score
 from scipy.stats import kurtosis
 from scipy.stats import skew
 from sklearn.ensemble import IsolationForest
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
 
-# data load 
-train = pd.read_csv('data/2019_performance.csv')
-rate = pd.read_csv('data/2019_rating.csv')
+## data load 
+perform = pd.read_csv('data/2019_performance.csv')
+rating = pd.read_csv('data/2019_rating.csv')
 test = pd.read_csv('data/question.csv')
 
-# detect outlier
+## detect outlier
 """
 x : dataset
-p : ratio of deleted outlier
+p : ratio of deleted outlier(0 ~ 1)
 return : dataset that dropped outlier
 """
 def del_outlier(x,p):
@@ -30,6 +32,3 @@ def del_outlier(x,p):
     sns.boxplot(tem[tem['anomaly']==1]['취급액'])
     dist(tem[tem['anomaly']==1]['취급액'])
     return train[tem['anomaly']==1]
-
-
-
