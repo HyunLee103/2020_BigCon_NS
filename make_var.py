b340243db3426ff421b0aad911112175b8535d81
HyelinNAM <lightsalt28@naver.com>
2020-08-27 16:34:48 +0900

    integrate class

--- make_var.py
+++ make_var.py
@@ -1,307 +1,238 @@
 import numpy as np
 import pandas as pd
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 import copy
 
-# 추가 정보 데이터프레임 따로 생성. performance(data) 데이터프레임과 인덱스로 매칭
-class mk_additional_info():
+class mk_var():
 
-    '''
-    input: perform, rating
-    return: (__call__) additional info dataframe (주문량,방송ID,시청률,시청률_방송별)
-    '''
-
-    def __init__(self,data,rating):
-        self.data = data 
-        self.rating = rating
-        self.info = pd.DataFrame()
-
-    def mk_order(self):
-        return round(self.data['취급액']/self.data['판매단가'])
-
-    def str_to_datetime(self,date):
-
-        if date:
-            return self.data['방송일시'].map(lambda x: datetime.strptime(x,'%Y-%m-%d %H:%M'))
-
-        else: # for rating['시간대]
-            return self.rating['시간대'].map(lambda x: datetime.strptime(x,'%H:%M').time())
-
-    def mk_showid(self):
-        def preprocess(name):
-            name = name.replace('무이자','')
-            name = name.replace('일시불','')
-            name = name.strip()
-
-            return name.split(' ')[0]
-
-        tmp = self.data['상품명'].map(preprocess)
+    def __init__(self):
+        self.data = pd.read_csv('data/2019_performance.csv', encoding='utf-8')
+        self.rating = pd.read_csv('data/2019_rating.csv',encoding='utf-8')
         
-        names = tmp.tolist()
-        ids = [0]
+        # str to datetime
+        self.data['방송일시'] = self.data['방송일시'].map(lambda x: datetime.strptime(x,'%Y-%m-%d %H:%M'))
+        self.rating['시간대'] = self.rating['시간대'].map(lambda x: datetime.strptime(x,'%H:%M').time())
+        
+        self.data['length'] = copy.deepcopy(self.data['노출(분)']).replace(0,method='ffill')
 
-        for i, name in enumerate(names[1:],1):
-            prior = names[i-1]
+        self.info = self.mk_addtional_info()
 
-            if prior == name:
-                ids.append(ids[-1])
-            else:
-                ids.append(ids[-1]+1)
+    def mk_addtional_info(self):
+        
+        def mk_order():
+            return round(self.data['취급액']/self.data['판매단가'])
 
-        return np.array(ids)
-    
-    def mk_rating(self,by_show=False):
-        self.data['노출(분)_수정'] = copy.deepcopy(self.data['노출(분)']).replace(0,method='ffill')
+        def mk_show(): # 상품명 기준
+            def preprocess(name):
+                name = name.replace('무이자','')
+                name = name.replace('일시불','')
+                name = name.strip()
 
-        def cal_ratio(date,long):
+                return name.split(' ')[0]
 
-            time_idx = dict(zip(self.rating.시간대,rating.index)) 
-            # key - 시간대(str), value - 인덱스
+            tmp = self.data['상품명'].map(preprocess)
 
-            # 예외처리
-            if (date.date().year == 2020):
-                return 0
+            names = tmp.tolist()
+            ids = [0]
 
-            else:
-                rating_for_day = self.rating.loc[:,['시간대',str(date.date())]]
+            for i, name in enumerate(names[1:],1):
+                prior = names[i-1]
 
-                start_idx = time_idx[date.time()]
-                end = date + timedelta(minutes = long)
-                end_idx = time_idx[end.time()]
+                if prior == name:
+                    ids.append(ids[-1])
+                else:
+                    ids.append(ids[-1]+1)
 
-            return self.rating.loc[start_idx:end_idx,str(date.date())].mean()
+            return np.array(ids)
+        
+        def mk_rating():
 
-        if by_show:
-            if '시청률' in self.info.columns:
+            time_idx = dict(zip(self.rating.시간대,self.rating.index)) 
+
+            def cal_ratio(date,length):
+               
+                if (date.date().year == 2020):
+                   return 0
+
+                else:
+                    rating_for_day = self.rating.loc[:,['시간대',str(date.date())]]
+            
+                    start_idx = time_idx[date.time()]
+                    end = date + timedelta(minutes=length)
+                    end_idx = time_idx[end.time()]
+
+                return self.rating.loc[start_idx:end_idx, str(date.date())].mean()
+
+            return self.data.apply(lambda x: cal_ratio(x['방송일시'],x['length']),axis=1)
+
+        def mk_rating_byshow(info):
+
+            if 'rating' in info.columns:
                 pass
-
             else:
-                self.info['시청률'] = self.data.apply(lambda x: cal_ratio(x['방송일시'],x['노출(분)_수정']) ,axis=1)
+                self.data['rating'] = mk_rating()
+
+            show_rating = dict(info.groupby('show_id')['rating'].mean())
+                
+            return info['show_id'].map(lambda x: show_rating[x])
             
-            
-            showid_rating = dict(self.info.groupby('방송ID')['시청률'].mean())
+        info = pd.DataFrame()
+        info['order'] = mk_order()
+        info['show_id'] = mk_show()
+        info['rating'] = mk_rating()
+        info['rating_byshow'] = mk_rating_byshow(info)
 
-            return self.info['방송ID'].map(lambda x: showid_rating[x])
+        return info
 
-        else:
-            return self.data.apply(lambda x: cal_ratio(x['방송일시'],x['노출(분)_수정']) ,axis=1)
-        
-    def __call__(self): # 모든 함수 실행
+    def mk_datetime_var(self):
 
-        self.data['방송일시'] = self.str_to_datetime(date=True)
-        self.rating['시간대'] = self.str_to_datetime(date=False)
+        def mk_day():
+            return self.data['방송일시'].map(lambda x: x.weekday())
 
-        self.info['주문량'] = self.mk_order()
-        self.info['방송ID'] = self.mk_showid()
-        self.info['시청률'] = self.mk_rating(by_show=False)
-        self.info['시청률_방송별'] = self.mk_rating(by_show=True)
+        def mk_holiday():
+            return self.data['방송일시'].map(lambda x: 1 if x.weekday() > 4 else 0)
 
-        return self.info
-
-class mk_datetime_var():
-    
-    '''
-    input: perform
-    return: (__call__) 요일, 휴일 여부, 방송시간-시, 방송시간-분
-    '''
-
-    def __init__(self,data):
-        self.data = data
-        
-    def mk_weekday(self): # 요일
-        return self.data['방송일시'].map(lambda x: x.weekday())
-
-    def mk_holiday(self): # 휴일 여부
-        return self.data['방송일시'].map(lambda x: 1 if x.weekday() >4 else 0)
-
-    def mk_hour(self,grouping=False): # 방송 시간 - 시
-
-        def hour_grouping(hour): # 주문량 기준으로 인간지능 그룹핑
-            if 1 <= hour < 7: # 1, 2, 6
-                return 0
-            elif 7 <= hour < 9:
-                return 1
-            elif 9 <= hour < 15:
-                return 2
-            elif 15 <= hour < 19:
-                return 3
-            else:
-                return 4
-        
-        if grouping:
-            return self.data['방송일시'].map(lambda x: hour_grouping(x.hour))
-        
-        else:
+        def mk_hour():
             return self.data['방송일시'].map(lambda x: x.hour)
 
-    def mk_min(self,grouping=False): # 방송 시간 - 분
-        
-        def min_grouping(min): 
-            return min # 그룹핑 기준 정해야
+        def mk_hour_group():
 
-        if grouping:
-            return self.data['방송일시'].map(lambda x: min_grouping(x.minute))
+            def hour_grouping(hour): # 주문량 기준으로 인간지능 그룹핑
+                if 1 <= hour < 7: # 1, 2, 6
+                    return 0
+                elif 7 <= hour < 9:
+                    return 1
+                elif 9 <= hour < 15:
+                    return 2
+                elif 15 <= hour < 19:
+                    return 3
+                else:
+                    return 4
+            
+            return self.data['방송일시'].map(lambda x: hour_grouping(x.hour))
         
-        else:
+        def mk_min():
             return self.data['방송일시'].map(lambda x: x.minute)
 
-    def __call__(self):
+        def mk_min_group():
 
-        self.data['방송요일'] = self.mk_weekday()
-        self.data['휴일'] = self.mk_holiday()
-        self.data['방송시각'] = self.mk_hour(grouping=False)
-        self.data['방송분'] = self.mk_min(grouping=True)
+            def min_grouping(min):
+                if min <20:
+                    return 0
+                elif min == 20:
+                    return 1
+                else:
+                    return 2
 
-        return self.data
-
-
-class mk_showtime_var():
-    '''
-    input: perform
-    return: (__call__) 노출(분)_그룹
-    '''
-
-    def __init__(self,data):
-        self.data = data
-        self.data['노출(분)_수정'] = copy.deepcopy(self.data['노출(분)']).replace(0,method='ffill')
-
-    def grouping(self):
-
-        def showtime_grouping(showtime): # '노출(분) > 24개'
-            if showtime < 20:
-                return 0
-            elif showtime == 20:
-                return 1
-            else:
-                return 2
-
-        return self.data['노출(분)_수정'].map(showtime_grouping)
-
-
-    def __call__(self):
-        self.data['노출(분)_그룹'] = self.data['노출(분)_수정'].map(self.grouping())
-
-        return self.data
-
-class mk_mcode_var():
-    '''
-    input: perform
-    return: (__call__) 마더코드_빈도(=마더코드 기준 방송빈도)
-    '''
-
-    def __init__(self,data,info):
-        self.data = data
-        self.info = info
-
-    def mk_mcode_freq(self): 
-        # 마더코드에 따른 방송횟수.(유사 브랜드파워) 
-        # 한 방송에 마더코드 여러개일 수 있음. > 둘 다 카운팅 되어야
-
-        self.info['마더코드'] = copy.deepcopy(self.data['마더코드'])
-        tmp = self.info.groupby('방송ID')['마더코드'].apply(lambda x: list(set(x))).reset_index(name='마더코드')
-        mcode_showfreq = dict(pd.Series(sum([mcode for mcode in tmp.마더코드],[])).value_counts())
-
-        return self.data['마더코드'].map(lambda x: mcode_showfreq[x])
-
-    def __call__(self):
-        self.data['마더코드_빈도'] = self.mk_mcode_freq()
-
-        del info['마더코드']
+            return self.data['방송일시'].map(lambda x: min_grouping(x.minute))
         
+        self.data['day'] = mk_day()
+        self.data['holiday'] = mk_holiday()
+        self.data['hour'] = mk_hour()
+        self.data['hour_gr'] = mk_hour_group()
+        self.data['min'] = mk_min()
+        self.data['min_gr'] = mk_min_group()
+
         return self.data
 
-class mk_pcode_var():
-    '''
-    input: perform
-    return: (__call__) 상품코드_빈도(=상품코드 기준 방송빈도)
-    '''
-    def __init__(self,data,info):
-        self.data = data
-        self.info = info
+    def mk_length_var(self):
 
-    def mk_pcode_freq(self):
-        self.info['상품코드'] = copy.deepcopy(self.data['상품코드'])
-        tmp = self.info.groupby('방송ID')['상품코드'].apply(lambda x: list(set(x))).reset_index(name='상품코드')
-        pcode_showfreq = dict(pd.Series(sum([pcdoe for pcdoe in tmp.상품코드],[])).value_counts())
+        def mk_length_grouping():
 
-        return self.data['상품코드'].map(lambda x: pcode_showfreq[x])
+            def length_grouping(length):
+                if length < 20:
+                    return 0
+                elif length == 20:
+                    return 1
+                else:
+                    return 2
+
+            return self.data['length'].map(length_grouping)
+
+        self.data['len_gr'] = mk_length_grouping()
+
+        return self.data
+
+    def mk_mcode_var(self):
+
+        def mk_mcode_freq():
+            self.info['mcode'] = copy.deepcopy(self.data['마더코드'])
+            tmp = self.info.groupby('show_id')['mcode'].apply(lambda x: list(set(x))).reset_index(name='mcode')
+            mcode_freq = dict(pd.Series(sum([mcode for mcode in tmp.mcode],[])).value_counts())
+
+            return self.data['마더코드'].map(lambda x: mcode_freq[x])
+
+        def mk_mcode_freq_grouping():
+
+            def freq_grouping(freq):
+                if freq <= 5:
+                    return 0 # 4225개
+
+                elif 5< freq <= 17:
+                    return 1 # 6157개
+
+                elif 17< freq <= 30:
+                    return 2 # 6197개
+                
+                elif 30< freq <= 51:
+                    return 3 # 7334개
+
+                elif 51< freq <= 130 :
+                    return 4 # 6469개
+    
+                else:
+                    return 5 # 6990개
+
+            return self.data['mcode_freq'].map(freq_grouping)
+        
+        self.data['mcode_freq'] = mk_mcode_freq()
+        self.data['mcode_freq_gr'] = mk_mcode_freq_grouping()
+
+        return self.data
+
+    def mk_pcode_var(self):
+
+        def mk_pcode_freq():
+            self.info['pcode'] = copy.deepcopy(self.data['상품코드'])
+            tmp = self.info.groupby('show_id')['pcode'].apply(lambda x: list(set(x))).reset_index(name='pcode')
+            pcode_freq = dict(pd.Series(sum([pcode for pcode in tmp.pcode],[])).value_counts())
+
+            return self.data['상품코드'].map(lambda x: pcode_freq[x])
+
+        def mk_pcode_count():
+            tmp = self.info.groupby('show_id')['pcode'].apply(lambda x: len(set(x)))
+            pcode_count = dict(tmp)
+
+            return self.info['show_id'].map(lambda x: pcode_count[x])
+
+        self.data['pcode_freq'] = mk_pcode_freq()
+        self.data['pcode_count'] = mk_pcode_count()
+
+        return self.data
+
+    def mk_rating_var(self):
+
+        def mk_rating():
+            return copy.deepcopy(self.info['rating'])
+
+        def mk_rating_byshow():
+            return copy.deepcopy(self.info['rating_byshow'])
+
+        self.data['rating'] = mk_rating()
+        self.data['rating_byshow'] = mk_rating_byshow()
+
+        return self.data
 
     def __call__(self):
-        self.data['상품코드_빈도'] = self.mk_pcode_freq()
 
-        del info['상품코드']
-
-        return self.data
-
-class mk_rating_var():
-    '''
-    input: perform
-    return: (__call__) 시청률, 방송시청률
-    '''
-    def __init__(self,data,info):
-        self.data = data
-        self.info = info
-
-    def mk_rating(self):
-        return copy.deepcopy(self.info['시청률'])
-
-    def mk_rating_byshow(self):
-        return copy.deepcopy(self.info['시청률_방송별'])
-
-    def __call__(self):
-
-        self.data['시청률'] = self.mk_rating()
-        self.data['시청률_방송별'] = self.mk_rating_byshow()
+        self.data = self.mk_datetime_var()
+        self.data = self.mk_length_var()
+        self.data = self.mk_mcode_var()
+        self.data = self.mk_pcode_var()
+        self.data = self.mk_rating_var()
 
         return self.data
 
+var = mk_var()
+data = var()
 
-# 사용
-data = pd.read_csv('data/2019_performance.csv', encoding='utf-8') # 실제론 test와 concat해야
-rating = pd.read_csv('data/2019_rating.csv',encoding='utf-8')
-
-additional_info = mk_additional_info(data,rating)
-info = additional_info()
-info
-
-date = mk_datetime_var(data)
-date() # ~ = date() 할당 안해도 data 변해있음. copy.deepcopy(data) 사용해야?
-
-# test셋에 마더코드, 상품 코드 겹치는 것 적어서 걱정. 
-# 상품명 전처리 거친 걸로 다시 빈도 뽑아볼수도? (브랜드, 상품명)
-
-showtime = mk_showtime_var(data)
-showtime()
-
-mcode = mk_mcode_var(data,info)
-mcode()
-
-pcode = mk_pcode_var(data,info)
-pcode()
-
-ratings = mk_rating_var(data,info)
-ratings()
-
-# 다른 변수와 시청률 관계
-day_rating = pd.DataFrame(data.groupby('방송요일')['시청률'].mean().reset_index())
-day_rating.plot.scatter(x='방송요일', y='시청률')
-
-hour_rating = pd.DataFrame(data.groupby('방송시각')['시청률'].mean()).reset_index()
-hour_rating.plot.scatter(x='방송시각', y='시청률')
-
-min_rating = pd.DataFrame(data.groupby('방송분')['시청률'].mean()).reset_index()
-min_rating.plot.scatter(x='방송분', y='시청률')
-
-mfreq_rating = pd.DataFrame(data.groupby('마더코드_빈도')['시청률'].mean()).reset_index()
-mfreq_rating[mfreq_rating['마더코드_빈도']<500].plot.scatter(x='마더코드_빈도',y='시청률',c='DarkBlue')
-
-pfreq_rating = pd.DataFrame(data.groupby('상품코드_빈도')['시청률'].mean()).reset_index()
-pfreq_rating[pfreq_rating['상품코드_빈도']<500].plot.scatter(x='상품코드_빈도',y='시청률',c='DarkBlue')
-
-order_rating = pd.DataFrame(info.groupby('주문량')['시청률'].mean()).reset_index()
-order_rating.plot.scatter(x='주문량',y='시청률')
-order_rating[order_rating['주문량']<6000].plot.scatter(x='주문량',y='시청률')
-order_rating[order_rating['주문량']<3000].plot.scatter(x='주문량',y='시청률')
-
-info.groupby('방송ID')['시청률'].mean().describe()
-info[info['시청률_방송별']> 0.08] # 손질 오징어, 진공 스텐 냄비
