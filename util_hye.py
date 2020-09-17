3b58f5e3cadba9cb8995c8167061a333cccbec24
HyelinNAM <lightsalt28@naver.com>
2020-09-17 22:38:46 +0900

    for predict using show id

---
+++ util_hye.py
@@ -0,0 +1,326 @@
+#-*-coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas.core.reshape.reshape import get_dummies
+import seaborn as sns
+plt.rc('font', family='NanumBarunGothic')
+from sklearn.ensemble import IsolationForest
+from sklearn.cluster import KMeans
+from make_var_func import mk_var
+from sklearn.preprocessing import LabelEncoder
+import os
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
+
+## load data
+def load_data(data_path,weather=False,trend=False):
+    perform_raw = pd.read_csv(os.path.join(f'{data_path}','2019_performance.csv'))
+    test = pd.read_csv(os.path.join(f'{data_path}','question.csv'))
+    rating = pd.read_csv(os.path.join(f'{data_path}','2019_rating.csv'),encoding='utf-8')
+
+    if weather:
+        weather_train = pd.read_csv(os.path.join(f'{data_path}','weather_data.csv'))
+        weather_test = pd.read_csv(os.path.join(f'{data_path}','weather_data_test.csv'))
+        perform_raw = pd.concat([perform_raw,weather_train],axis=1)
+        test = pd.concat([test,weather_test],axis=1)
+    if trend:
+        trend_train = pd.read_csv(os.path.join(f'{data_path}','shopping_trend.csv'))
+        trend_test = pd.read_csv(os.path.join(f'{data_path}','shopping_trend_test.csv'))
+        perform_raw = pd.concat([perform_raw,trend_train],axis=1)
+        test = pd.concat([test,trend_test],axis=1)
+
+    perform_raw.reset_index(inplace=True)
+    perform_raw.rename(columns={'index':'id'},inplace=True)
+
+    test.reset_index(inplace=True)
+    test.rename(columns={'index':'id'},inplace=True)
+    test['id'] = test['id'].map(lambda x: x + perform_raw.loc[perform_raw.index[-1],'id'])
+
+    return perform_raw, rating, test
+
+
+## detect outlier
+"""
+x : dataset
+p : ratio of deleted outlier(0 ~ 1)
+return : dataset that dropped outlier
+"""
+def del_outlier(x,p):
+    clf=IsolationForest(contamination=float(p),
+                        max_features=1.0, bootstrap=False, n_jobs=-1, random_state=2020, verbose=0)
+    tem = x['취급액'].reset_index()
+    clf.fit(tem)
+    tem['anomaly'] = clf.predict(tem)
+    return x[tem['anomaly']==1]
+
+
+## clustering
+"""
+x : dataset
+k : # of cluster
+return : 기존 dataset에 kmeans라는 컬럼 추가한 df
+"""
+def km_clust(x, k, var=['취급액'],inner=3):
+
+    Z = x[var].reset_index()
+    km = KMeans(n_clusters=k,random_state=2020)
+    km.fit(Z)
+    Z['kmeans'] = km.labels_
+
+    # inner cluster
+    if inner!=0:
+        object = Z[Z['kmeans']==1]
+        K = Z[Z['kmeans']==1]['취급액'].reset_index()
+        left = Z[Z['kmeans']!=1]
+        km = KMeans(n_clusters=inner,random_state=2020)
+        km.fit(K)
+        object['labels'] = km.labels_
+        object['kmeans'][object['labels']==0]= 1
+        object['kmeans'][object['labels']==1]= 3
+        object['kmeans'][object['labels']==2]= 4
+        object.drop(['labels'],axis=1,inplace=True)
+        object.set_index('index')
+        left.set_index('index')
+        res = pd.concat([object.set_index('index'),left.set_index('index')]).sort_index()
+
+        return pd.concat([x,res['kmeans']],axis=1)
+    else:
+        return pd.concat([x,Z['kmeans']],axis=1)
+
+#def km_clust_hye(x, k):
+
+
+def preprocess(train,test,drop_rate,k):
+    perform = del_outlier(train,drop_rate)
+    perform.reset_index(drop=True, inplace=True)
+    perform = km_clust(perform, k, inner=3)
+    X_km = perform.drop(['kmeans','취급액'],axis=1)
+    y_km = perform[['kmeans']]
+    y = perform[['취급액']]
+    y.rename(columns={'취급액':'sales'},inplace=True)
+    test_km = test.drop(['취급액'],axis=1)
+    data = pd.concat([X_km,test_km]) # 합쳐서 전처리
+    data.reset_index(inplace=True,drop=True)
+    
+    var = mk_var(data)
+    data = var()
+
+    return data, y, y_km
+
+def make_sid(data):
+    def preprocess(name):
+
+        # 예외 처리
+        name = name.replace('휴롬퀵스퀴저','휴롬 퀵스퀴저')
+        name = name.replace('장수흙침대','장수 흙침대')
+        name = name.replace('1+1 국내제조', '국내제조')
+
+        words = ['보국미니히터', '우아미', '갓바위', '두씽', '해뜰찬', '법성포굴비', '쥐치포', '공간아트', '르젠','쿠쿠']
+
+        # name 이 words 안에 있다면 name 전체를 위의 word로 대체.
+        for word in words:
+            if (word in name):
+                name = word
+
+        if ('삼성' in name) and ('도어' in name):
+            name = 'tmp'
+
+        brands = ['프라다','구찌','버버리','코치','마이클코어스','톰포드', '페라가모', '생로랑']
+
+        for brand in brands:
+            name = name.replace(brand,'명품')
+
+        # 상품명 처리
+        words = ['LG전자','삼성','LG','(일)3인용', '(일)4인용', '(무)3인용', '(무)4인용', '(삼성카드 6월 5%)',\
+        '[1세트]','[2세트]','[SET]','[풀패키지]','[실속패키지]', '(점보특대형)','(점보형)', '(중형)',\
+        '(퀸+퀸)','(킹+싱글)','(퀸+싱글)','(킹사이즈)','(퀸사이즈)','(더블사이즈)','(싱글사이즈)','(싱글+싱글)','(더블+더블)','(더블+싱글)',\
+        '(점보)','(특대)','(대형)','더커진','(1등급)467L_','(1등급)221L_','1세트 ','2세트 ','5세트 ','19년 신제품 ',\
+        'K-SWISS 남성','K-SWISS 여성','(퀸)','(싱글)','[무이자]','[일시불]','(무)','(일)','무)','일)','무이자','일시불']
+
+        for word in words:
+            name = name.replace(word,'')
+        
+        name = name.strip()
+
+        return name.split(' ')[0]
+
+    tmp = data['상품명'].map(preprocess)
+    names = tmp.tolist()
+    ids = [0]
+
+    for i, name in enumerate(names[1:],1):
+        prior = names[i-1]
+
+        if prior == name:
+            ids.append(ids[-1])
+        else:
+            ids.append(ids[-1]+1)
+
+    return np.array(ids)    
+
+def preprocess_hye(train,test,drop_rate,k):
+    #perform = del_outlier(train,drop_rate)
+    #perform.reset_index(drop=True, inplace=True)
+
+    # 방송 ID 부여 perform, test
+    data = pd.concat([perform,test])
+    data.reset_index(inplace=True,drop=True)
+    data['show_id'] = make_sid(data)
+
+    len_train = len(perform)
+
+    perform = data.iloc[:len_train,:].reset_index(drop=True)
+    test = data.iloc[len_train:,:].reset_index(drop=True)
+
+    p_idsales = pd.DataFrame(perform.groupby('show_id')['취급액'].sum()).reset_index()
+    p_idsales = km_clust(p_idsales, k, inner=0)
+
+    sid_k = p_idsales['kmeans'].to_dict()
+
+    # perform에 방송별로 구한 kmeans 값 부여
+    perform['kmeans'] = perform['show_id'].map(lambda x: sid_k[x])
+
+    X_km = perform.drop(['kmeans','취급액'],axis=1)
+    y_km = perform[['kmeans']]
+    y = perform[['취급액']]
+
+    y.rename(columns={'취급액':'sales'},inplace=True)
+
+    test_km = test.drop(['취급액'], axis=1)
+
+    data = pd.concat([X_km,test_km])
+    data.reset_index(inplace=True,drop=True)
+
+    var = mk_var(data)
+    data = var()
+    
+    return data, y, y_km
+
+
+def preprocess(train,test,drop_rate,k):
+    perform = del_outlier(train,drop_rate)
+    perform.reset_index(drop=True, inplace=True)
+    perform = km_clust(perform, k, inner=3)
+    X_km = perform.drop(['kmeans','취급액'],axis=1)
+    y_km = perform[['kmeans']]
+    y = perform[['취급액']]
+    y.rename(columns={'취급액':'sales'},inplace=True)
+    test_km = test.drop(['취급액'],axis=1)
+    data = pd.concat([X_km,test_km]) # 합쳐서 전처리
+    data.reset_index(inplace=True,drop=True)
+    
+    var = mk_var(data)
+    data = var()
+
+    return data, y, y_km
+
+
+def mk_trainset(data,dummy = ['gender','pay','hour_gr','min_gr','len_gr','show_norm_order_gr']):
+    """
+    select feature to make train set 
+    arg : data, dummy(list that make it one-hot-encoding)
+    return : data
+    """
+    data['sales_per'] = np.log1p(data['판매단가'])
+    data.rename(columns={'마더코드':'mcode','상품군':'cate','노출(분)':'length_raw','상품코드':'item_code'},inplace=True)
+
+    encoder = LabelEncoder()
+    encoder.fit(data['cate'])
+    data['cate'] = encoder.transform(data['cate'])
+
+    all_cate = ['day','hour','min','mcode_freq_gr','show_order','gender','pay','hour_gr','min_gr','len_gr','show_order','show_norm_order_gr','cate']
+    left_cate = [x for x in all_cate if x not in dummy]
+
+    if dummy:
+        data = pd.get_dummies(data,columns=(dummy))
+
+    if left_cate != []:
+        for var in left_cate:
+            data[var] = data[var].astype('category')
+
+    data['mcode'] = data['mcode'].astype('str').apply(lambda x: x[3:])
+    data['mcode'] = data['mcode'].astype(int)
+    data['item_code'] = data['item_code'].astype('str').apply(lambda x: x[2:])
+    data['item_code'] = data['item_code'].astype(int)
+    data = data.drop(['방송일시','상품명','판매단가','length_raw'],axis=1)
+
+    # group_by show id
+    
+
+    return data
+
+def mk_trainset_hye(data):
+    data.rename(columns={'마더코드':'mcode','상품군':'cate','노출(분)':'length_raw','상품코드':'item_code'},inplace=True)
+    
+    encoder = LabelEncoder()
+    encoder.fit(data['cate'])
+    data['cate'] = encoder.transform(data['cate'])
+
+    data['mcode'] = data['mcode'].astype('str').apply(lambda x: x[3:])
+    data['mcode'] = data['mcode'].astype(int)
+    data['item_code'] = data['item_code'].astype('str').apply(lambda x: x[2:])
+    data['item_code'] = data['item_code'].astype(int)
+
+    data = data.drop(['방송일시','상품명','판매단가','length_raw','hour_gr','min_gr','len_gr','mcode_freq_gr','rating','gender','pay','set','special','show_order','show_norm_order','show_norm_order_gr'],axis=1)
+
+
+
+    return data
+
+
+def metric(real, pred):
+    tem = np.abs((real - pred)/real)
+    return tem.mean() * 100
+
+def s_metric(real, pred):
+    tem = np.abs(pred - real)/((np.abs(pred) + np.abs(real))/2)
+    return tem.mean() * 100
+
+
+def feature_impo(model,data):
+    feature_imp = pd.DataFrame(sorted(zip(model.feature_importances_,data.columns)), columns=['Value','Feature'])
+    plt.figure(figsize=(20, 10))
+    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value", ascending=False))
+    plt.title('LightGBM Features (avg over folds)')
+    plt.tight_layout()
+    plt.show()
+    plt.savefig('lgbm_importances-01.png')
+
+
+def scoring(sales,pred):
+    if sales==0:
+        score = s_metric(sales,pred)
+        return score
+    else:
+        score = metric(sales,pred)
+        return score
+
+
+
+def kmeans(data,k,var=['sales'],visual=False):
+
+    train_km = pd.concat([data.iloc[:36250,:],y],axis=1)
+    Z = train_km[var].reset_index()
+
+    km = KMeans(n_clusters=k,random_state=2020)
+    km.fit(Z)
+    df = pd.DataFrame(Z)
+    df['kmeans'] = km.labels_
+    df = inner_cluster(df,3,1)
+    y_km = df['kmeans']
+
+    if visual:
+        pca3 = PCA(n_components=3)
+        data_pca3 = pca3.fit_transform(scaled_Z)
+        fig = plt.figure(figsize=(12, 8))
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(data_pca3[:,0], data_pca3[:,1], data_pca3[:,2], c=df['kmeans'], s=60, edgecolors='white')
+        ax.set_title('3D of Target distribution by diagnosis')
+        ax.set_xlabel('pc1')
+        ax.set_ylabel('pc2')
+        ax.set_zlabel('pc3')
+        plt.show()
+
+    return y_km
