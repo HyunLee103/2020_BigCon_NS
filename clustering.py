c59e66722beba04f476f6242b5a4ab9ea7eaca3f
SangHyung-Jung <cukirhkwk@hanyang.ac.kr>
2020-09-28 10:44:00 +0900

    cluster_rnd_search_done

--- clustering.py
+++ clustering.py
@@ -19,7 +19,12 @@ def clustering(data,y_km,train_len,test=False):
     s = robustScaler.fit(np.array(X_train['sales']).reshape(-1,1))
     X_train['sales'] = robustScaler.transform(np.array(X_train['sales']).reshape(-1,1))
 
-    lgb = LGBMClassifier(n_estimators=1500,learning_rate=0.04,subsample=0.8,colsample_bytree=0.5,random_state=2020,objective='multiclass')
+    best_params = {'bagging_fraction': 0.9392683888838689, 'feature_fraction': 0.25673627673971944, 'importance_type': 'split',
+    'lambda_l1': 0.9589781997564626, 'lambda_l2': 0.27902813458237496, 'learning_rate': 0.7308044021474812,
+    'max_bin': 13, 'max_depth': 3, 'min_child_samples': 2, 'min_data_in_leaf': 1, 'min_gain_to_split': 0.2028734422783811,
+    'min_sum_hessian_in_leaf': 5, 'n_estimators': 1000, 'num_leaves': 43}
+
+    lgb = LGBMClassifier(boosting_type='gbdt', random_state=2020, objective='multiclass', **best_params)
     
     if test:
         lgb.fit(X_train.drop(['id','sales']+['is_mcode','mcode_freq','mcode_freq_gr','mcode_sales_mean','mcode_sales_std','mcode_sales_med','mcode_sales_rank','mcode_order_mean','mcode_order_med','mcode_order_rank','mcode_order_std'],axis=1),y_km)
@@ -27,7 +32,9 @@ def clustering(data,y_km,train_len,test=False):
         X_test.reset_index(drop=True,inplace=True)
         train = pd.concat([X_train,y_km],axis=1)
         train.reset_index(drop=True,inplace=True)
+
         return train, X_test, s
+
     else:      
         train_features, val_features, train_labels, val_labels = train_test_split(X_train,y_km,random_state=2020,test_size=0.08)
 
@@ -36,7 +43,6 @@ def clustering(data,y_km,train_len,test=False):
         val_0 = val_features.drop(val_1.index)
         val_0['is_mcode'] = 0
         val_features = pd.concat([val_0,val_1])
-
 
         # val cluster 예측
         lgb.fit(train_features.drop(['id','sales','is_mcode'],axis=1),train_labels,verbose=False)
